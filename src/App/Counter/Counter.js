523fda8ea4834af6ce431b2170f5e233dc4a7075
agnieszkajankowska <ajankowska0404@gmail.com>
2017-05-21 21:53:51 +0200

    Counter component - smart component

--- src/App/Counter/Counter.js
+++ src/App/Counter/Counter.js
@@ -1,14 +1,35 @@
 import React from 'react'
-import { ProgressBar } from 'react-bootstrap'
+import {ProgressBar, Button} from 'react-bootstrap'
 
 
 export class Counter extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            value: 0,
+            minValue: 0,
+            maxValue: 20
+        }
+    }
+
     render() {
+
+        this.increaseBarValue = () =>
+            this.setState({value: Math.min(this.state.value + 1, this.state.maxValue)})
+
+        this.decreaseBarValue = () =>
+            this.setState({value: Math.max(this.state.value - 1, this.state.minValue)})
+
+        const computedStyle = this.state.value < 5 ? "success" : (this.state.value > 15 ?
+        "danger" : "warning")
+
         return (
             <div>
                 <h1>Counter</h1>
-                <p>Value is:</p>
-                <ProgressBar bsStyle="danger" now={50} />
+                <p>Value is: {this.state.value}</p>
+                <ProgressBar active bsStyle={computedStyle} now={this.state.value} max={this.state.maxValue}/>
+                <Button bsStyle="primary" onClick={this.increaseBarValue}>Increase</Button>
+                <Button bsStyle="primary" onClick={this.decreaseBarValue}>Decrease</Button>
             </div>
         )
     }
