539ca82a6f5faa1960a648bc00937de1cbe3064c
agnieszkajankowska <ajankowska0404@gmail.com>
2017-05-21 14:52:05 +0200

    Counter progress

--- src/App/App.js
+++ src/App/App.js
@@ -1,15 +1,16 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import {Counter} from '../App/Counter/Counter'
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-          <h1>Hello world</h1>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <h1>Hello world</h1>
+                <Counter />
+            </div>
+        );
+    }
 }
 
 export default App;
--- src/App/Counter/Counter.js
+++ src/App/Counter/Counter.js
@@ -1,3 +1,17 @@
-/**
- * Created by Agnieszka on 2017-05-14.
- */
+import React from 'react'
+import { ProgressBar } from 'react-bootstrap'
+
+
+export class Counter extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Counter</h1>
+                <p>Value is:</p>
+                <ProgressBar bsStyle="danger" now={50} />
+            </div>
+        )
+    }
+}
+
+export default Counter
