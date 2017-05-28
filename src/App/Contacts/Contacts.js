5930058398bd11c427ff53c238b6f7b42203d47f
agnieszkajankowska <ajankowska0404@gmail.com>
2017-05-28 16:36:53 +0200

    Adding form to contacts

--- src/App/Contacts/Contacts.js
+++ src/App/Contacts/Contacts.js
@@ -1,19 +1,43 @@
 import React from 'react'
-
+import {Button, Form, FormGroup, FormControl, ControlLabel} from 'react-bootstrap'
 
 
 export class Contacts extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            contacts: ''
+        }
+    }
 
     componentWillMount(){
         console.log('component will mount')
         fetch(process.env.PUBLIC_URL + '/data/contacts.json')
-            .then(response => response.json()).then(data => console.log(data))
+            .then(response => response.json())
+            .then(data => this.setState({contacts: data}))
     }
 
     render() {
+        console.log("state.contacts", this.state.contacts)
         return (
             <div>
                 <h1>Contacts</h1>
+                <Form horizontal>
+                    <FormGroup controlId="formInlineName">
+                        <ControlLabel>Name</ControlLabel>
+                        {' '}
+                        <FormControl type="text" placeholder="Enter contact name" />
+                    </FormGroup>
+                    {' '}
+                    <FormGroup controlId="formInlineSurname">
+                        <ControlLabel>Surname</ControlLabel>
+                        {' '}
+                        <FormControl type="text" placeholder="Enter contact surname" />
+                    </FormGroup>
+                    <Button type="submit">
+                        Add contact
+                    </Button>
+                </Form>
             </div>
         )
     }
